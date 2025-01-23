85e512e2af22b27d4413f388b841dfbaaefbe382
nomadisop <>
2025-01-23 15:10:20 +0530

    images

--- navbar.php
+++ navbar.php
@@ -3,7 +3,7 @@
        <nav class="navbar navbar-expand-lg navbar-light">
                 <div class="container-fluid">
                 <a class="navbar-brand d-flex align-items-center ms-4" href="../home/">
-                    <img src="images/navlogo.png" class="d-inline-block align-top ms-2" alt="" style="max-height: 20vh;">
+                    <img src="../images/navlogo.png" class="d-inline-block align-top ms-2" alt="" style="max-height: 20vh;">
                     <span style="font-family: Montserrat, sans-serif; font-weight: 800; font-style: italic; color: white;" class="ms-2">KOKANI RISHTA</span>
                 </a>
                 
